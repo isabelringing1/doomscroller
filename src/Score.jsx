c9ebf253b875c19be37e27df815d79457230a453
Isabel Lee <isabelringing1@gmail.com>
2026-06-27 09:54:49 -0400

    score

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 export default function Score() {
   const score = useSelector((s) => s.game.score)
+  const prevScoreRef = useRef(score)
+  const [pulsing, setPulsing] = useState(false)
 
-  return <div className="score">Score: {score}</div>
+  useEffect(() => {
+    if (score !== prevScoreRef.current) {
+      setPulsing(true)
+    }
+    prevScoreRef.current = score
+  }, [score])
+
+  return (
+    <div className={`score${pulsing ? ' score--pulse' : ''}`}>
+      Score:{' '}
+      <span
+        className={`score-value${pulsing ? ' score-value--pulse' : ''}`}
+        onAnimationEnd={() => setPulsing(false)}
+      >
+        {score}
+      </span>
+    </div>
+  )
 }
